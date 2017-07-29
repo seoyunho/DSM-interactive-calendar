acbf815c4dc615a0c5352f8438c8d8ebc6062b03
river-stone-jin <sukjinjjang@gmail.com>
2017-07-30 02:44:57 +0900

    uncomment response sendfile and delete log

--- server/routes/post/router.js
+++ server/routes/post/router.js
@@ -2,10 +2,10 @@ let express = require('express');
 let router = express.Router();
 let manager = require('./manager');
 let fs= require('fs');
+let path = require('path');
 
-router.route('/html').get(function(req,res){
-    // res.sendFile(path.resolve(__dirname,'../','../','./public/html/index.html'))
-    console.log(path.resolve(__dirname,'../','../','./public/html/index.html'));
+router.route('/').get(function(req,res){
+    res.sendFile(path.resolve(__dirname,'../','../','./public/html/index.html'));
 });
 
 module.exports=router;
